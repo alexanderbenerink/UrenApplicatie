403b7f9d7e6c1d96e06b93679c6d148b6635fa2b
adb <39517675+alexanderbenerink@users.noreply.github.com>
2019-05-09 17:42:41 +0000

    submenu template

---
+++ _URENAPP_test/submenu/landing.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin="anonymous">
+    <!-- Override Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Override Bootstrap JS -->
+    <script src="js/main.js"></script>
+</head>
+<body>
+
+<!-- Navigation -->
+  <nav>
+  	<!-- Side navigation -->
+  	<div id="mySidenav" class="sidenav">
+  	  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  	  <a href="#">Log in</a>
+  	  <a href="#">Registreer</a>
+  	</div>
+  	<!-- Header navigation -->
+  	<div id="main">
+  	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  	  <ul class="navbar-nav">
+  	  	<li class="nav-item">
+  	  		<span class="nav-link" style="font-size:15px;cursor:pointer" onclick="openNav()">&#9776;</span>
+  	  	</li>
+        <li>
+          <img src="images/trentonlogo.png" class="col-md mu-2" style="width: 150px; height 200px;">
+        </li>
+  	    <li class="nav-item">
+  	      <a class="nav-link" href="landing.php">Urenadministratie</a> 
+  	    </li>
+  	    <li class="nav-item">
+  	      <a class="nav-link" href="#">Projectbeheer</a>
+  	    </li>
+  	    <li class="nav-item">
+  	      <a class="nav-link disabled" href="#">Accountbeheer</a>
+  	    </li>
+  	  </ul>
+      </nav>
+  	
+    <!-- Hier kan de content van de website, zodat de content naar links geduwd kan worden zodra het submenu uitschuift. -->
+      
+    </div>
+  </nav>
+
+
+
+
+<!-- Other JavaScript -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"crossorigin="anonymous"></script>
+  </body>
+</html>
