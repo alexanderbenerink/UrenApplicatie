ac4d11fa72d8601e9690fc72ea016b07791ceb5d
adb <39517675+alexanderbenerink@users.noreply.github.com>
2019-05-09 19:46:06 +0000

    Update main.js
    time class toegevoegd

--- _URENAPP_test/js/main.js
+++ _URENAPP_test/js/main.js
@@ -7,3 +7,12 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+var time = document.getElementsByClassName('time');
+for (var i = 0; i < time.length; i++) { 
+  time[i].addEventListener('keyup', function (e) {; 
+        var reg = /[0-9]/;
+        if (this.value.length == 2 && reg.test(this.value)) this.value = this.value + ":";  
+        if (this.value.length > 5) this.value = this.value.substr(0, this.value.length - 1);
+    });
+};
